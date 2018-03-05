bb5399219f71185c0d3eacd74e311dd15f421454
Seab <honeyboyseab@gmail.com>
2018-03-05 13:54:10 -0500

    implemnted in view did load how to capture the metadata for qr codes

--- QRCodeReader/QRScannerController.swift
+++ QRCodeReader/QRScannerController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
-class QRScannerController: UIViewController {
+class QRScannerController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
     @IBOutlet var messageLabel:UILabel!
     @IBOutlet var topbar: UIView!
     
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // get the back facing camera for capturing videos
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera] , mediaType: AVMediaType.video, position: .back)
+        
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera")
+            return
+        }
+        
+        do {
+            // get an instance of the AVCaptureDeviceInput class using the previous device object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            // set the input device on the caputure session
+            captureSession?.addInput(input)
+            
+            // initialize a AVCaptureMetadataOutput object and set it as the output device
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        } catch {
+            print(error)
+            return
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -23,16 +51,5 @@ class QRScannerController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
